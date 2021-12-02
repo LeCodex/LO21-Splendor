cd67eb34373fa1db0af59c64e5b467f0b6b281d5
Fanthommm <mcfanthom@gmail.com>
2021-12-02 15:14:07 +0100

    Début du code de player

--- src/player.cpp
+++ src/player.cpp
@@ -1,1 +1,19 @@
 #include "player.h"
+
+int Player::getPoint() {
+    int result=0;
+    for (auto i=0;i<3;i++){
+        result+=getRessourceCard(i).prestige;
+    }
+    return result;
+}
+
+int *Player::getBonuses() {
+    int bonuses[5];
+    for (auto i=0;i<3;i++){
+        for (auto j=0;j<5;j++){
+        bonuses[j]+=getRessourceCard(i).cost[j];
+    }
+    return bonuses;
+}
+
--- src/player.h
+++ src/player.h
@@ -1,4 +1,26 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#endif
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Player {
+private:
+    const string name;
+    Bank bank;
+    const RessourceCard** cards[3];
+    const NobleCard** nobles;
+public:
+    Player(string s, int n) : name(s){};
+    ~Player();
+    Action play(vector<Action> actions);
+    Bank getBank() {return bank;}
+    string getName() {return name;}
+    RessourceCard getRessourceCard(int i) {return cards[i]};
+    int* getBonuses();
+    int getPoint();
+    };
